27f1f656a9349b7c784847b2db7337eb6b7ac5b5
neetance <ankitbasu14@gmail.com>
2024-10-17 00:07:24 +0530

    wrote code for getting the price of token A wrt token B for uniswap

--- src/priceFetcher.sol
+++ src/priceFetcher.sol
@@ -5,6 +5,8 @@ import {DEX} from "../src/arbrMaster.sol";
 import {IVault} from "../node_modules/@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import {IManagedPool} from "../node_modules/@balancer-labs/v2-interfaces/contracts/pool-utils/IManagedPool.sol";
 import {IERC20} from "../node_modules/@balancer-labs/v2-interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
+import {IUniswapV2Factory} from "../lib/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Pair} from "../lib/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract PriceFetcher {
     address balancerVault;
@@ -33,7 +35,29 @@ contract PriceFetcher {
     function getUniswapPrice(
         address tokenA,
         address tokenB
-    ) internal returns (uint256) {}
+    ) internal view returns (uint256) {
+        IUniswapV2Factory uniswapv2Factory = IUniswapV2Factory(uniswapv2);
+        address pairAddr = uniswapv2Factory.getPair(tokenA, tokenB);
+        IUniswapV2Pair uniswapV2pair = IUniswapV2Pair(pairAddr);
+
+        address token0 = uniswapV2pair.token0();
+        //address token1 = uniswapV2pair.token1();
+
+        (uint256 reserve0, uint256 reserve1, ) = uniswapV2pair.getReserves();
+        uint256 balanceA;
+        uint256 balanceB;
+
+        if (tokenA == token0) {
+            balanceA = reserve0;
+            balanceB = reserve1;
+        } else {
+            balanceA = reserve1;
+            balanceB = reserve0;
+        }
+
+        uint256 price = (balanceB * 1e18) / balanceA;
+        return price;
+    }
 
     function getSushiswapPrice(
         address tokenA,
